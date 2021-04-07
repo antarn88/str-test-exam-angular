c172310f41d9d1e61fe4eebc909ca589f4ed310f
Arnold <antaloczi.arnold@gmail.com>
2021-04-07 19:59:08 +0200

    Updated product model and product service

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,2 +1,6 @@
 export class Product {
+  id: number = 0;
+  name: string = '';
+  price: number = 0;
+  category: string = '';
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  serverAddress: string = 'https://nettuts.hu/jms/antarn88/products';
+
+  productsList$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): void {
+    this.productsList$.next([]);
+    this.http.get<Product[]>(this.serverAddress).subscribe(
+      productsList => this.productsList$.next(productsList),
+      error => console.error(error)
+    );
+  }
+
+  get(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.serverAddress}/${id}`);
+  }
+
+  update(product: Product): Observable<Product> {
+    return this.http.patch<Product>(`${this.serverAddress}/${product.id}`, product);
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.serverAddress, product);
+  }
+
+  remove(id: number): Observable<Product> {
+    return this.http.delete<Product>(`${this.serverAddress}/${id}`);
+  }
+
 }
