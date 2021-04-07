2d8404197a8535a47e58c89e1a581fdec36ac19b
Arnold <antaloczi.arnold@gmail.com>
2021-04-07 20:08:32 +0200

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { ProductEditorComponent } from './page/product-editor/product-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'product/:id',
+    component: ProductEditorComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
